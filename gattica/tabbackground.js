6c87becf0af4b7e28de03ad3eec7aaa37747f78d
unknown <hjh2bs@virginia.edu>
2017-08-04 23:30:34 -0400

    adding persistent storage

--- gattica/tabbackground.js
+++ gattica/tabbackground.js
@@ -4,12 +4,46 @@
 var enable = true;
 var maxTab = 50;
 
+var storage = chrome.storage.local;
+
+
+storage.get('mytd_tab_enabled', function(value){
+    var content = Object.values(value)
+    //console.log(content[0])
+    if (content[0] == false){
+        enable = false;
+    }
+    else{
+        enable = true;
+    }
+});
+
+storage.get('mytd_max_tab', function(value){
+    var content = Object.values(value)
+    if (content[0]==undefined){
+        maxTab = 50;
+    }
+    else{
+        maxTab = content[0]
+    }
+})
+
+
+
 function setEnable(bool){
     enable = bool;
+    storage.set({'mytd_tab_enabled':bool}, function(){
+        console.log(bool)
+    });
 }
 
 function setMax(num){
     maxTab = num;
+    storage.set({'mytd_max_tab':num},function(){
+        console.log(num)
+    });
+
+
 }
 
 
